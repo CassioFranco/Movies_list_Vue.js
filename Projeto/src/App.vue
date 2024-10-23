d4710f564b32346dbd60843bcab1830666027d72
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-10-23 15:41:05 -0300

    Novo Home

--- Projeto/src/App.vue
+++ Projeto/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <Home/>
+    <!-- <Navbar /> -->
+    <Home />
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
+import Home from './components/Home.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {  
   name: 'App',
   components: {
-    Home
+    Home,
+    Navbar
   },
 
 }

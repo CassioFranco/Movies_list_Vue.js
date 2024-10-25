93e80b5b0aefb85b70231c39a3effd9d72b432cb
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-10-25 13:31:40 -0300

    Adicioando botão de favorito

--- Netflix/src/components/Home.vue
+++ Netflix/src/components/Home.vue
@@ -1,151 +1,76 @@
 <template>
-  <html lang="pt-br" data-bs-theme="auto">
+  <div lang="pt-br" data-bs-theme="auto">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="Bootstrap contributors">
+      <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
 
-  <head>
+    <body>
+      <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+      </svg>
+      <Navbar />
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.122.0">
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/album/">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-
-    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
-  </head>
-
-  <body>
-    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-      <symbol id="check2" viewBox="0 0 16 16">
-        <path
-          d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
-      </symbol>
-      <symbol id="circle-half" viewBox="0 0 16 16">
-        <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
-      </symbol>
-      <symbol id="moon-stars-fill" viewBox="0 0 16 16">
-        <path
-          d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
-        <path
-          d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z" />
-      </symbol>
-      <symbol id="sun-fill" viewBox="0 0 16 16">
-        <path
-          d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
-      </symbol>
-    </svg>
-
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-      <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button"
-        aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)">
-        <svg class="bi my-1 theme-icon-active" width="1em" height="1em">
-          <use href="#circle-half"></use>
-        </svg>
-        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-      </button>
-
-      <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light"
-            @click="changeTheme('light')" aria-pressed="false">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em">
-              <use href="#sun-fill"></use>
-            </svg>
-            Light
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark"
-            @click="changeTheme('dark')" aria-pressed="false">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em">
-              <use href="#moon-stars-fill"></use>
-            </svg>
-            Dark
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto"
-            @click="changeTheme('auto')" aria-pressed="true">
-            <svg class="bi me-2 opacity-50" width="1em" height="1em">
-              <use href="#circle-half"></use>
-            </svg>
-            Auto
-          </button>
-        </li>
-      </ul>
-
-    </div>
-    <Navbar />
-    <main>
-
-      <section class="py-4 text-center container">
-        <div class="row py-lg-3">
-          <div class="col-lg-6 col-md-8 mx-auto">
-            <img src="../../public/images/Netflix_Logo_RGB.png" alt="logo" width="500">
-            <p class="lead text-body-secondary">Bem-vindo à nossa página de catálogo de filmes! Descubra novas
-              histórias aqui cada filme conta uma história, e estamos aqui para ajudar você a encontrá-las!</p>
-            <p>
-              <a href="#" class="teste btn btn-primary my-2">Contato</a>
-              <a href="#" class="btn btn-secondary my-2">Favoritos</a>
-            </p>
+      <main>
+        <section class="py-4 text-center container">
+          <div class="row py-lg-3">
+            <div class="col-lg-6 col-md-8 mx-auto">
+              <img src="../../public/images/Netflix_Logo_RGB.png" alt="logo" width="500">
+              <p class="lead text-body-secondary">Bem-vindo à nossa página de catálogo de filmes!</p>
+              <p>
+                <a href="#" class="btn btn-primary my-2">Contato</a>
+                <a href="#" class="btn btn-secondary my-2">Favoritos</a>
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-
-      <div class="album py-5 bg-body-tertiary">
-        <div class="container">
-          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 d-flex justify-content-center">
-            <div v-for="movie in movies" :key="movie.id" class="col d-flex justify-content-center">
-              <div class="card shadow-sm" style="width: 18rem;">
-                <img :src="getPosterUrl(movie.poster_path)" class="img-fluid rounded-start" :alt="movie.title"
-                  style="width: 100%;">
-                <h5 class="card-title">{{ movie.title }}</h5>
-                <div class="card-body">
-                  <p class="card-text movie-description">{{ movie.overview }}</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button @click="goToMovie(movie.id)">Ver mais</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Favoritar</button>
+        </section>
+        <div class="album py-5 bg-body-tertiary">
+          <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 d-flex justify-content-center">
+              <div v-for="movie in movies" :key="movie.id" class="col d-flex justify-content-center">
+                <div class="card shadow-sm" style="width: 18rem;">
+                  <img :src="getPosterUrl(movie.poster_path)" class="img-fluid rounded-start" :alt="movie.title">
+                  <h5 class="card-title">{{ movie.title }}</h5>
+                  <div class="card-body">
+                    <p class="card-text movie-description">{{ movie.overview }}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button @click="goToMovie(movie.id)">Ver mais</button>
+                        <button @click="toggleFavorite(movie)" class="favorite-button" aria-label="Favoritar">
+                          <img :src="movie.isFavorite ? '/heart%20(1).png' : '/heart.png'"
+                               :alt="movie.isFavorite ? 'favorite' : 'not favorite'"
+                               class="favorite-icon" />
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-
-
-    </main>
-    <Footer />
-  </body>
-
-  </html>
-
+      </main>
+      <Footer />
+    </body>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
-import Movies from './Movies.vue';
 import Navbar from './Navbar.vue';
 import Footer from './Footer.vue';
-
-
 
 export default {
   name: 'Home',
   components: {
-    Movies,
     Navbar,
     Footer
   },
   data() {
     return {
       movies: [],
-      selectedMovie: null,
       currentTheme: 'auto'
     };
   },
@@ -161,7 +86,10 @@ export default {
       axios
         .get(url)
         .then((response) => {
-          this.movies = response.data.results;
+          this.movies = response.data.results.map(movie => ({
+            ...movie,
+            isFavorite: false
+          }));
         })
         .catch((error) => {
           console.error(error);
@@ -171,8 +99,10 @@ export default {
       return `https://image.tmdb.org/t/p/w500${path}`;
     },
     goToMovie(movieId) {
-
       this.$router.push({ name: 'movies', params: { id: movieId } });
+    },
+    toggleFavorite(movie) {
+      movie.isFavorite = !movie.isFavorite;
     },
     changeTheme(theme) {
       this.currentTheme = theme;
@@ -180,117 +110,30 @@ export default {
     },
     updateTheme(theme) {
       const htmlElement = document.documentElement;
-
-      if (theme === 'light') {
-        htmlElement.setAttribute('data-bs-theme', 'light');
-      } else if (theme === 'dark') {
-        htmlElement.setAttribute('data-bs-theme', 'dark');
-      } else {
-        htmlElement.removeAttribute('data-bs-theme');
-      }
+      htmlElement.setAttribute('data-bs-theme', theme === 'auto' ? '' : theme);
     }
   }
 };
 </script>
 
-
-
 <style>
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  user-select: none;
-}
-
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
-  }
-}
-
-.b-example-divider {
-  width: 100%;
-  height: 3rem;
-  background-color: rgba(0, 0, 0, .1);
-  border: solid rgba(0, 0, 0, .15);
-  border-width: 1px 0;
-  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-}
-
-.b-example-vr {
-  flex-shrink: 0;
-  width: 1.5rem;
-  height: 100vh;
-}
-
-.bi {
-  vertical-align: -.125em;
-  fill: currentColor;
-}
-
-.nav-scroller {
-  position: relative;
-  z-index: 2;
-  height: 2.75rem;
-  overflow-y: hidden;
-}
-
-.nav-scroller .nav {
-  display: flex;
-  flex-wrap: nowrap;
-  padding-bottom: 1rem;
-  margin-top: -1px;
-  overflow-x: auto;
-  text-align: center;
-  white-space: nowrap;
-  -webkit-overflow-scrolling: touch;
-}
-
-.btn-bd-primary {
-  --bd-violet-bg: #712cf9;
-  --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-  --bs-btn-font-weight: 600;
-  --bs-btn-color: var(--bs-white);
-  --bs-btn-bg: var(--bd-violet-bg);
-  --bs-btn-border-color: var(--bd-violet-bg);
-  --bs-btn-hover-color: var(--bs-white);
-  --bs-btn-hover-bg: #6528e0;
-  --bs-btn-hover-border-color: #6528e0;
-  --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-  --bs-btn-active-color: var(--bs-btn-hover-color);
-  --bs-btn-active-bg: #5a23c8;
-  --bs-btn-active-border-color: #5a23c8;
-}
-
 .bd-mode-toggle {
   z-index: 1500;
 }
 
-.bd-mode-toggle .dropdown-menu .active .bi {
-  display: block !important;
+.favorite-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
 }
 
-.card {
-  width: 300px;
-}
-
-.card img {
-  width: 100%;
-}
-
-.album .container {
-  display: flex;
-  justify-content: center;
-}
-
-.row {
-  justify-content: center;
-}
-
-.teste {
-  margin-right: 15px;
+.favorite-icon {
+  width: 24px;
+  height: 24px;
 }
 
 .movie-description {
@@ -300,13 +143,5 @@ export default {
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 3;
-}
-
-.container {
-  color: white;
-}
-
-.voltar {
-  color: #e9292c;
 }
 </style>
